9fea302b782f1655e2741bdcf927c3a63f97d971
K-KY <kyuyoungk@naver.com>
2024-08-18 13:07:03 +0900

    Feat : Product insert 다른 dto사용 하도록 변경

--- Muzi/src/main/java/com/Toy2/product/db/dao/ProductDao.java
+++ Muzi/src/main/java/com/Toy2/product/db/dao/ProductDao.java
@@ -1,6 +1,7 @@
 package com.Toy2.product.db.dao;
 
 import com.Toy2.product.db.dto.ProductDto;
+import com.Toy2.product.db.dto.request.ProductInsertRequestDto;
 import com.Toy2.product.db.dto.request.ProductPageRequestDto;
 import com.Toy2.product.db.dto.request.ProductUpdateRequestDto;
 
@@ -11,6 +12,7 @@ public interface ProductDao {
     int count();
 
     boolean insert(ProductDto build);
+    boolean insert(ProductInsertRequestDto build);
 
     ProductDto select(int productNumber);
     List<ProductDto> selectPage(ProductPageRequestDto pageRequestDto);
