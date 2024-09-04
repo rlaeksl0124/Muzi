ee51b073bb7aafb146622872a7923724c67d6043
alswooy <108874185+alswooy@users.noreply.github.com>
2024-09-04 13:45:17 +0900

    2024.09.04 redis session 추가

--- Muzi/src/main/java/com/Toy2/Cust/Controller/LoginController.java
+++ Muzi/src/main/java/com/Toy2/Cust/Controller/LoginController.java
@@ -5,18 +5,15 @@ import com.Toy2.Cust.Domain.CustDto;
 import com.Toy2.Cust.Service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -57,7 +54,7 @@ public class LoginController {
         try {
             /* 로그인실패: 로그인체크를통해 email과 pwd를 받아서 DB에 있는 정보와 일치한지 확인 */
             /* 관리자 로그인시 일치하지않으면 메시지 출력후 login 페이지로 redirect */
-            if("admin".equals(c_email)){
+            if("admin".equals(c_email) || "admin2".equals(c_email)){
                 if(!adminLogin(c_pwd)){
                     rattr.addFlashAttribute("msg", "관리자 비밀번호가 일치하지 않습니다.");
                     return "redirect:/login";
@@ -131,13 +128,14 @@ public class LoginController {
 
 
     /* admin 계정 로그인 */
-    @Value("#{confidential['admin.password']}")
+    @Value("#{confidential['admin.password']}, #{confidential['admin2.password']}")
     private String adminPwd;
     private boolean adminLogin(String c_pwd){
 
         try {
             /* admin 계정을 select */
             CustDto admin = custDao.selectEmail("admin");
+            CustDto admin2 = custDao.selectEmail("admin2");
 
             /* admin의 DB 정보에있는 pwd가 암호회되지 않았다면 암호화하는 로직 */
             if(!admin.getC_pwd().startsWith("$2a$")){
@@ -145,6 +143,14 @@ public class LoginController {
                 String hashPwd = passwordService.encodePassword(rawPwd);
                 admin.setC_pwd(hashPwd);
                 custDao.updateCust(admin);
+            }
+
+            /* admin의 DB 정보에있는 pwd가 암호회되지 않았다면 암호화하는 로직 */
+            if(!admin2.getC_pwd().startsWith("$2a$")){
+                String rawPwd = adminPwd;
+                String hashPwd = passwordService.encodePassword(rawPwd);
+                admin2.setC_pwd(hashPwd);
+                custDao.updateCust(admin2);
             }
 
             /* 암호화된 pwd가 DB와 일치한지 확인 */
